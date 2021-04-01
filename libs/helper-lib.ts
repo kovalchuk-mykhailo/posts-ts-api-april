0740ee40fb1441c1429fb138002a8e138e6f3b51
annpryshchepa <pryshchepa.hanna@gmail.com>
2021-04-01 21:58:34 +0300

    added comments endpoint

--- libs/helper-lib.ts
+++ libs/helper-lib.ts
@@ -1,11 +1,23 @@
 import { Post } from "src/interfaces/Post";
+import { Comment } from 'src/interfaces/Comment';
 
 /**
  * Sort array of Posts by Date in the descending and ascending ways.
  * @param arr - array of Posts.
  * @param isDescending - boolean value for specify sorting way. Default way = ascending
  */
+
 export const sortPostsByDate = (arr: Post[], isDescending: boolean) => {
+  return arr.sort((a, b) => {
+    if (isDescending) {
+      return a.createdAt > b.createdAt ? -1 : a.createdAt < b.createdAt ? 1 : 0;
+    }
+
+    return a.createdAt < b.createdAt ? -1 : a.createdAt > b.createdAt ? 1 : 0;
+  });
+};
+
+export const sortCommentsByDate = (arr: Comment[], isDescending: boolean) => {
   return arr.sort((a, b) => {
     if (isDescending) {
       return a.createdAt > b.createdAt ? -1 : a.createdAt < b.createdAt ? 1 : 0;
